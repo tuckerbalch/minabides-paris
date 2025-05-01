2d2a4aa86845ac5dfb189e9a588908c6a81c3489
David Byrd <d.byrd@bowdoin.edu>
2025-05-01 18:37:25 -0400

    Add RL utility classes: Box, Discrete, ReplayBuffer.

--- util.py
+++ util.py
@@ -1,5 +1,77 @@
+
+from collections import namedtuple
 import concurrent.futures, numpy as np, os, random, sys, torch
 from datetime import datetime as dt
+
+
+### Utility classes.
+
+class Box():
+    """ A very simple continuous space in the style of the gymnasium Box.
+        Does not support unbounded spaces. """
+    def __init__(self, low, high):
+        self.low, self.high = low, high
+        self.shape = self.low.shape
+
+    def sample(self):
+        """ Uniformly randomly samples from this bounded space. """
+        return np.random.uniform(low=self.low, high=self.high, size=self.shape)
+
+class Discrete():
+    """ A very simple space in the style of the gynmasium Discrete. """
+
+    def __init__(self, n, start = 0):
+        self.n, self.start = n, start       # length and starting number of action space range
+        self.shape = (1,)
+
+    def sample(self):
+        """ Uniformly randomly samples from this space. """
+        return np.random.randint(low=self.start, high=self.start+self.n)
+
+# Simple named tuple type for replay samples.
+ReplaySample = namedtuple('ReplaySample', ['observations', 'actions', 'next_observations', 'rewards'])
+
+class ReplayBuffer():
+    """ A very simple replay buffer in the style of stable-baselines3. """
+    def __init__(self, maxlen, obs_shape, act_shape, discrete_actions = False):
+        self.s = np.empty((maxlen, *obs_shape), dtype=np.float32)
+        self.a = np.empty((maxlen, *act_shape), dtype=np.int64 if discrete_actions else np.float32)
+        self.s_prime = np.empty((maxlen, *obs_shape), dtype=np.float32)
+        self.r = np.empty((maxlen), dtype=np.float32)
+
+        self.n = 0
+        self.full = False
+
+    def add(self, s, a, s_prime, r):
+        self.s[self.n] = np.array(s)
+        self.a[self.n] = np.array(a)
+        self.s_prime[self.n] = np.array(s_prime)
+        self.r[self.n] = np.array(r)
+
+        self.n = self.n + 1 % self.s.shape[0]
+        if self.n == 0: self.full = True
+
+    def sample(self, num_samples):
+        idx = np.random.randint(0, self.s.shape[0] if self.full else self.n, size=num_samples)
+        return ReplaySample(torch.tensor(self.s[idx]), torch.tensor(self.a[idx]),
+                            torch.tensor(self.s_prime[idx]), torch.tensor(self.r[idx]))
+
+class TeeOutput:
+    """ Used by main simulation to automatically tee output to a file. """
+    def __init__(self, tee_path):
+        self.screen = sys.stdout
+        self.file = open(tee_path, "wb", buffering=0)
+
+    def write(self, text):
+        self.screen.write(text)
+        self.file.write(text.encode('utf-8'))
+
+    def flush(self):
+        """ For filestream compatibility. """
+        pass
+
+
+### Utility methods.
 
 def clamp(n, smallest, largest):
     """ Clamp numeric value n to be within inclusive range [smallest,largest]. """
@@ -45,18 +117,4 @@ def sign (n):
     elif n > 0: return 1
     else: return -1
 
-
-class TeeOutput:
-    """ Used by main simulation to automatically tee output to a file. """
-    def __init__(self, tee_path):
-        self.screen = sys.stdout
-        self.file = open(tee_path, "wb", buffering=0)
-
-    def write(self, text):
-        self.screen.write(text)
-        self.file.write(text.encode('utf-8'))
-
-    def flush(self):
-        """ For filestream compatibility. """
-        pass
 
