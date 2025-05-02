78570fdc4e9eb28872658b408cae4da68104d26d
David Byrd <d.byrd@bowdoin.edu>
2025-05-02 14:18:04 -0400

    Rearranged hp_search.sh to be easier to use.
    Updated .gitignore to allow a local directory that is not tracked.

--- hp_search.sh
+++ hp_search.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#SBATCH --output=results/%x-%j.out  # Output file
-#SBATCH --error=results/%x-%j.err   # Error file
-#XBATCH --exclusive=user	    # Do not share nodes with other users.
+#SBATCH --output=results/.slurm/%x-%j.out  # Output file
+#SBATCH --error=results/.slurm/%x-%j.err   # Error file
 
 ### Adapt to your cluster configuration.
 #SBATCH --nodes=6
@@ -9,16 +8,20 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=5GB
 
-### Can handle any number of permutations and run "tasks" number at a time,
+### Can handle any number of permutations and run ntasks at a time,
 ### queuing automatically, so no parameters needed except an identifier.
 ### Just put everything in this script's for loops.
 
+### Additional search hyperparameters can be added by following the
+### example of those present.  The cross product of all combinations
+### of parameters will be run a number of times equal to the repeat loop.
+
 if [ $# -lt 1 ]; then
-  echo $0: 'usage: sbatch <this_script> <run_tag>'
+  echo $0: 'usage: sbatch <this_script> <user_tag>'
   exit 1
 fi
 
-runid="$1"
+usertag="$1"
 
 # Source your virtual environment.
 source ./venv/bin/activate
@@ -26,43 +29,60 @@ source ./venv/bin/activate
 # Initialize task num for separate log file generation.
 tasknum=1
 
-# How many background agents of each type should be included?
-# (ddpg_td3 example only, background example is hardcoded)
-bg_mkt=1      # market maker
-bg_mom=5      # momentum
-bg_nse=10     # noise
-bg_obi=1      # order book imbalance
-bg_val=10     # value
+# Record a single timestamp for the entire batch.
+ts=$EPOCHSECONDS
 
 ### To avoid race conditions, when running a new date or time
 ### for the first time, consider a single run per combination
 ### of symbol + date + time, with a single training trip to
 ### cache all new data files.
 
-# How many times to repeat each configuration in the search.
-repeats=({1..1})     # for caching or installation testing
-#repeats=({1..192})  # how many times to repeat each experiment
+### Multiple values can be given as a range, e.g.
+### for value in {5..15}; do
+### or as a list, e.g.
+### for level in 10 20 30; do
 
-# The hyperparameters over which you want to search.
-symbols=("aapl" "intc" "msft")
-dates=("2019-12-30")
+# No indentation due to extremely deep nesting.
 
-for repeat in "${repeats[@]}";
-do
-  for symbol in "${symbols[@]}";
-  do
-    for date in "${dates[@]}";
-    do
-      # Example reinforcement learning experiment.
-      #srun -u --exclusive=user --mem-per-cpu 5GB -N1 -n1 /usr/bin/time -v python -u run_exp.py --exp ddpg_td3 --train_dates $date --val_dates $date --test_dates $date --symbol $symbol --bg_mkt $bg_mkt --bg_mom $bg_mom --bg_nse $bg_nse --bg_obi $bg_obi --bg_val $bg_val --models 1 --trips 5 --tag "${runid}_${symbol}_${repeat}_${tasknum}" &
+for repeat in {1..32}; do	# ALL: how many times to repeat every hp combination
 
-      # Example background agent experiment.
-      srun -u --exclusive=user --mem-per-cpu 5GB -N1 -n1 /usr/bin/time -v python -u run_exp.py --exp background --train_dates $date --symbol $symbol --trips 5 --tag "${runid}_${symbol}_${repeat}_${tasknum}" &
-      tasknum=$((tasknum + 1))
-    done
-  done
-done
+for date in 2019-12-30; do	# ALL: assumes train, val, test on one date
+for level in 10; do		# ALL: LOB levels to offer agents (and log)
+for lobintvl in 5e9; do		# ALL: how often to update LOB snapshots (and log)
+for seqlen in 20; do		# ALL: how many recent LOB snapshots to offer agents
+for symbol in AAPL; do		# ALL: one symbol per simulation
+for trip in 1; do		# ALL: trips through training period
+
+for bg_mkt in 0; do		# BG: how many market makers?
+for bg_mom in 0; do		# BG: how many momentum traders?
+for bg_nse in 0; do		# BG: how many noise traders?
+for bg_obi in 0; do		# BG: how many order book imbalance traders?
+for bg_val in 0; do		# BG: how many value traders?
+
+for rlagent in td3; do		# RL: which learning agent to use
+for embed in 3; do		# RL: embedding size for encoder
+for encoder in lstm; do		# RL: which sequence encoder to use
+for lr in 5e-1; do		# RL: learning rate
+for model in 1; do		# RL: how many models to alternate while training
+for tau in 0.2; do		# RL: target network rate of drift towards actor network
+for trade in 10; do		# RL: maximum size of one order
+for transcost in 0.00; do	# RL: transaction cost (fraction of transaction value)
+
+for expnoise in 0.5; do		# TD3: exploration noise
+for polnoise in 0.5; do		# TD3: policy noise
+for polfreq in 10; do		# TD3: policy update frequency (in steps)
+
+for trainfreq in 5; do		# DQN: training frequency (in steps)
+for targetfreq in 40; do	# DQN: target update frequenct (in steps)
+
+    # Example reinforcement learning experiment.
+    srun -u --exclusive=user --mem-per-cpu 5GB -N1 -n1 /usr/bin/time -v python -u run_exp.py --exp rl --rlagent $rlagent --expnoise $expnoise --polnoise $polnoise --polfreq $polfreq --train_freq $trainfreq --target_freq $targetfreq --train_dates $date --val_dates $date --test_dates $date --symbol $symbol --bg_mkt $bg_mkt --bg_mom $bg_mom --bg_nse $bg_nse --bg_obi $bg_obi --bg_val $bg_val --lr $lr --fixed --models $model --trips $trip --seqlen $seqlen --lobintvl $lobintvl --levels $level --trade $trade --encoder $encoder --embed $embed --tau $tau --trans_cost $transcost --ts $ts --tag $usertag --runtag "${symbol}_${repeat}_${tasknum}" &
+
+    tasknum=$((tasknum + 1))
+
+done; done; done; done; done; done; done; done; done; done
+done; done; done; done; done; done; done; done; done; done
+done; done; done; done; done
 
 wait
-      
 echo Done.
